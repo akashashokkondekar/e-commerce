1ffb8d03e3239dcf95745dda5c5921cb47dbdd8a
Akash Ashok Kondekar <akash@mentora.institute>
2025-02-09 19:44:22 +0100

    - Added Missing Vite Config files - Removed default icons and code - Integrated Redux Store in App - Added Basic Folder structure & Default Files

---
+++ e-commerce/react-based-e-commerce-website/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
+const router = createBrowserRouter([
+  
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
+);

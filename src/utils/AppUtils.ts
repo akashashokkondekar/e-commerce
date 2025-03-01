3db670d97d59a9cfe12d4c92fd102c857e4edb82
Akash Ashok Kondekar <akashkondekar00700@gmail.com>
2025-03-01 20:52:34 +0000

    Fixed: 1) Added Refresh button to refresh records. 2) Added Sort option to filter records based on your preference. 3) Optimized Product listing Logic.

--- src/utils/AppUtils.ts
+++ src/utils/AppUtils.ts
@@ -1,25 +1,49 @@
+import { ByNameFilterKeyOptionText, DefaultFilterKeyOptionText, FilterSortEnum, PriceHighToLowFilterKeyOptionText, PriceLowToHighFilterKeyOptionText } from "./AppConstant";
+
 export class AppUtils {
 
   static GetCurrencySymbolUsingCode(currencyCode: string) {
 
     let valueToReturn = "";
     switch (currencyCode) {
-  
+
       default:
         valueToReturn = "$";
         break;
-  
+
       case "USD":
       case "CAD":
         valueToReturn = "$";
         break;
     }
     return valueToReturn;
-  
+
   }
 
   static isValidateEmailAddress = (email: string) => {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
+  }
+
+  static getFilterKeyOptionText = (key: any) => {
+
+    var valueToReturn = DefaultFilterKeyOptionText;
+    switch (parseInt(FilterSortEnum[key])) {
+
+      case FilterSortEnum.Name:
+        valueToReturn = ByNameFilterKeyOptionText;
+        break;
+
+      case FilterSortEnum.Price_Low_To_High:
+        valueToReturn = PriceLowToHighFilterKeyOptionText;
+        break;
+
+      case FilterSortEnum.Price_High_To_Low:
+        valueToReturn = PriceHighToLowFilterKeyOptionText;
+        break;
+
+    }
+    return valueToReturn;
+  
   }
 }
